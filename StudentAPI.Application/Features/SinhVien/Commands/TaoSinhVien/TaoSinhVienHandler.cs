35a3605f230a3327949e7b84209428aa1c7d7c7a
phansytu <140830645+phansytu@users.noreply.github.com>
2026-09-20 17:53:17 +0700

    feat(sinh-vien): update TaoSinhVienHandler with new business rules

--- StudentAPI.Application/Features/SinhVien/Commands/TaoSinhVien/TaoSinhVienHandler.cs
+++ StudentAPI.Application/Features/SinhVien/Commands/TaoSinhVien/TaoSinhVienHandler.cs
@@ -1,6 +1,7 @@
 using AutoMapper;
 using MediatR;
 using StudentAPI.Application.Common.Interfaces;
+using StudentAPI.Application.Features.SinhVien.Rules;
 namespace StudentAPI.Application.Features.SinhVien.Commands.TaoSinhVien;
 
 public class TaoSinhVienHandler : IRequestHandler<TaoSinhVienCommand, int>
@@ -8,17 +9,22 @@ public class TaoSinhVienHandler : IRequestHandler<TaoSinhVienCommand, int>
     private readonly ISinhVienRepository _repository;
     private readonly IUnitOfWork _unitOfWork;
     private readonly IMapper _mapper;
+    private readonly ISinhVienBusinessRules _businessRules;
+
     public TaoSinhVienHandler(
         ISinhVienRepository repository,
         IUnitOfWork unitOfWork,
-        IMapper mapper)
+        IMapper mapper,
+        ISinhVienBusinessRules businessRules)
     {
         _repository = repository;
         _unitOfWork = unitOfWork;
         _mapper = mapper;
+        _businessRules = businessRules;
     }
     public async Task<int> Handle(TaoSinhVienCommand request, CancellationToken cancellationToken)
     {
+        await _businessRules.KiemTraTrungEmail(request.Email, excludeId: null, cancellationToken);
         var sinhVien = _mapper.Map<Domain.Entities.SinhVien>(request);
         await _repository.AddAsync(sinhVien, cancellationToken);
         await _unitOfWork.SaveChangesAsync(cancellationToken);
